31899e555dbae295f490d0934c7ef0f61d55f7d2
indian-music-arch <kamaleshp0406@gmail.com>
2020-01-07 12:43:35 +0530

    Upload all data files

---
+++ fetch.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+
+headers = {"Accept":"application/json", "Content-Type":"application/json", "Authorization":"Bearer token"}
+r = requests.get("https://api.spotify.com/v1/playlists/playlist)id", headers=headers)
+
+no_of_tracks = len(r.json()['tracks']['items'])
+for i in range(0, no_of_tracks):
+	preview_url = r.json()['tracks']['items'][i]['track']['preview_url']
+	print(preview_url)
+	file_name = r.json()['tracks']['items'][i]['track']['name']+".mp3"
+	if preview_url == None:
+		pass
+	else:
+		response = requests.get(preview_url)
+		try:
+			with open(file_name, 'wb') as f:
+				f.write(response.content)
+		except Exception as e:
+			print(str(e))
+
